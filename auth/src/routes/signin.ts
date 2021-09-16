a58d37d3af12d9241c0fd2b2c4ac6fa4f0c7b59f
La Ode Safar <laodesafar94@gmail.com>
2021-09-16 13:28:06 +0700

    membuat sign

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signin", (req, res) => {
-  res.send("Hi Dari Signin");
-});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .notEmpty()
+      .withMessage("You must supply a password"),
+  ],
+  async (req: Request, res: Response) => {
+    res.send("Hi Dari Signin");
+  }
+);
 
 export { router as signinRouter };
