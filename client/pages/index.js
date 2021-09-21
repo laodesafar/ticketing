11eb28686bb9e12c441224b21cba57faa8936ce7
La Ode Safar <laodesafar94@gmail.com>
2021-09-22 00:05:19 +0700

    init client menggunakan next dan tailwind

---
+++ client/pages/index.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <h1 className="bg-yellow-300 text-5xl font-bold  ">
+          Ini dari tailwind dan
+        </h1>
+      </div>
+    </div>
+  );
+}
